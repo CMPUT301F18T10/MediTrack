6da9b093b0574b63b365079faef6b98593b750aa
adithasan <adit@ualberta.ca>
2018-10-31 16:39:21 -0600

    Created the skeleton for the ProblemManagerService class, going to wait for data classes before merging

---
+++ app/src/main/java/com/example/meditrack/ProblemManagerService.java
@@ -0,0 +1,38 @@
+package com.example.meditrack;
+
+public class ProblemManagerService
+{
+    /* This class is responsible for operating on Problems and Records
+       This would include Add/Edit/Delete Problem and
+       Add/Delete Record
+
+       All Activities that needs to manipulate problems/records would
+       collect the necessary info from user and call this service to
+       update the ElasticSearch instance holding the information
+     */
+
+    public static void AddProblem(Problem problem)
+    {
+        // TODO: Finish the method
+    }
+
+    public static void EditProblem(Problem problem)
+    {
+        // TODO: Finish the method
+    }
+
+    public static void DeleteProblem(Integer problemId)
+    {
+        // TODO: Finish the method
+    }
+
+    public static void AddRecord(Integer problemId, Record record)
+    {
+        // TODO: Finish the method
+    }
+
+    public static void DeleteRecord(Integer problemId, Record record)
+    {
+        // TODO: Finish the method
+    }
+}
