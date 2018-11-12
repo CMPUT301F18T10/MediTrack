f5c65e8339de26745d2a05dcb4b8dc79bda5827d
lemonpopcorn <lemonpopcorn>
2018-11-11 21:51:49 -0700

    Fix crash in LoginActivity

--- app/src/main/java/com/example/meditrack/LoginActivity.java
+++ app/src/main/java/com/example/meditrack/LoginActivity.java
@@ -2,35 +2,38 @@ package com.example.meditrack;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
 import static com.example.meditrack.ApplicationManager.UserMode.CareGiver;
 
-public class loginActivity extends AppCompatActivity {
-    EditText userEmailEdit = (EditText) findViewById(R.id.loginEmail);
-    String userEmail = userEmailEdit.getText().toString();
-    RadioButton caretakerRadio = (RadioButton) findViewById(R.id.caretakerRadioButton);
+public class LoginActivity extends AppCompatActivity {
+
+    EditText userEmailEdit;
+    String userEmail;
+    RadioButton caretakerRadio;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        EditText userEmailEdit = (EditText) findViewById(R.id.loginEmail);
+        String userEmail = userEmailEdit.getText().toString();
+        RadioButton caretakerRadio = (RadioButton) findViewById(R.id.caretakerRadioButton);
+
     }
     public void loginClick(View v){
         if(caretakerRadio.isChecked()){
-            Intent caretakerIntent = new Intent(loginActivity.this, PatientsListActivity.class);
+            Intent caretakerIntent = new Intent(LoginActivity.this, PatientsListActivity.class);
             //caretakerIntent.putExtra("caretakerID", userEmail);
             ApplicationManager.LogIn(CareGiver, userEmail);
             startActivity(caretakerIntent);
         }
         else{
-            Intent patientIntent = new Intent(loginActivity.this, ProblemsListActivity.class);
+            Intent patientIntent = new Intent(LoginActivity.this, ProblemsListActivity.class);
             //patientIntent.putExtra("patientID", userEmail);
             ApplicationManager.LogIn(ApplicationManager.UserMode.Patient, userEmail);
             startActivity(patientIntent);
