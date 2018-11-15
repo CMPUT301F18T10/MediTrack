8a378bfa156890f358a67790d26d883bff3e00bd
RiXu2 <35273575+RiXu2@users.noreply.github.com>
2018-11-15 16:56:01 -0700

    Update MockDataRepositoryUserManager.java

--- app/src/main/java/com/example/meditrack/MockDataRepositoryUserManager.java
+++ app/src/main/java/com/example/meditrack/MockDataRepositoryUserManager.java
@@ -4,18 +4,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MockDataRepositoryUserManager extends DataRepositorySingleton {
-    private Patient patient;
+    private static Patient patient;
     private static CareProvider careProvider;
-    private ArrayList<String> bodyImageId = new ArrayList<String>(Arrays.asList("Img1223","Img1224","Img1225"));
-    private static ArrayList<String> patientId = new ArrayList<String>(Arrays.asList("12345678","22345678","32345678"));
-    public Patient getUser(ContactInfo contactInfo){
-        patient.userId = "12444538";
-        patient.setBodyLocationImageIds(bodyImageId);
+
+    public static Patient GetPatient()
+    {
+        String email = "testEmail@ualberta.ca";
+        String phoneNumber = "7808001557";
+        ArrayList<String> bodyImageId = new ArrayList<String>(Arrays.asList("Img1223","Img1224","Img1225"));
+        ContactInfo contactInfo = new ContactInfo(email,phoneNumber);
         patient.setContactInfo(contactInfo);
+        patient.setBodyLocationImageIds(bodyImageId);
         return patient;
     }
-    public static CareProvider getUser(){
-        careProvider.userId = "54726731";
+
+    public static CareProvider GetCareProvider()
+    {
+        ArrayList<String> patientId = new ArrayList<String>(Arrays.asList("12345678","22345678","32345678"));
         careProvider.setPatientIds(patientId);
         return careProvider;
     }
