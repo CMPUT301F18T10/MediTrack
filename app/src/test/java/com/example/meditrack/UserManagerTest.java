aef52188afb5031cd9b8ac40910c083f8538b29b
RIXU2 <rxu2@ualberta.ca>
2018-11-15 16:51:22 -0700

    user manager test

--- app/src/test/java/com/example/meditrack/UserManagerTest.java
+++ app/src/test/java/com/example/meditrack/UserManagerTest.java
@@ -1,4 +1,36 @@
 package com.example.meditrack;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserManagerTest {
+    @Test
+    public void testUserManager(){
+
+        // missing part: test addBodyLocationImage(), didn't do the body location image part yet
+        String email = "Email@ualberta.ca";
+        String phoneNumber = "7806554738";
+        UserManager userManager = new UserManager();
+        userManager.EditContactInfo(email, phoneNumber);
+        Patient patient = MockDataRepositoryUserManager.GetPatient();
+
+        //check EditContactInfo()
+        Assert.assertTrue("Edit method does not work in right way", email.equals(patient.getContactInfo().getEmail()));
+
+        //check checkBodyImageNumber()
+        Assert.assertTrue("check image number method does not work in right way", 3 == userManager.checkBodyImageNumber());
+
+        //check deleteBodyLocationImage()
+        userManager.deleteBodyLocationImage("Img1224");
+        Assert.assertTrue("delete image method does not work in right way",2 == userManager.checkBodyImageNumber());
+
+        //check addPatient()
+        String id = "22345878";
+        int lengthOfList = MockDataRepositoryUserManager.GetCareProvider().getPatientIds().size();
+        userManager.addPatient(id);
+        int new_length = MockDataRepositoryUserManager.GetCareProvider().getPatientIds().size();
+        Assert.assertTrue("add patient method does not work in right way", lengthOfList+1 == new_length );
+
+    }
+
 }
