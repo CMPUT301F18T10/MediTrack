e38cc9feee444608e632fd627963f469cab56269
RiXu2 <35273575+RiXu2@users.noreply.github.com>
2018-11-15 16:55:33 -0700

    Update UserManager.java

--- app/src/main/java/com/example/meditrack/UserManager.java
+++ app/src/main/java/com/example/meditrack/UserManager.java
@@ -5,51 +5,50 @@ import java.util.ArrayList;
 
 public class UserManager {
 	/* This class is responsible for operating on contact informations and patient
-       This would include Edit contactInfo, AddPatient to patient list, 
+       This would include Edit contactInfo, AddPatient to patient list,
        and Add comment
      */
 	private ContactInfo new_contactInfo;
 	private ArrayList<String> BodyImages;
+	private ArrayList<String> patientIds;
 
 	public void EditContactInfo(String email, String phoneNumber)
 	{
 		new_contactInfo.setEmail(email);
 		new_contactInfo.setPhoneNumber(phoneNumber);
-        DataRepositorySingleton.getUser("").setContactInfo(new_contactInfo);
+		MockDataRepositoryUserManager.GetPatient().setContactInfo(new_contactInfo);
 	}
-	
+
 	public void addPatient(String patientUserId)
 	{
-	    DataRepositorySingleton.getUser().AddPatientId(patientUserId);
+		patientIds = MockDataRepositoryUserManager.GetCareProvider().getPatientIds();
+		patientIds.add(patientUserId);
+		MockDataRepositoryUserManager.GetCareProvider().setPatientIds(patientIds);
+
 	}
 
 
-	public void addBodyLocationImage(Patient patient, ImageView image)
+	public void addBodyLocationImage(ImageView image)
 	{
-	    //missing part: upload image to database
+		//missing part: upload image to database
 		String imageId = String.valueOf(image.getTag());
-        DataRepositorySingleton.getUser("").getBodyLocationImages().add(imageId);
+		MockDataRepositoryUserManager.GetPatient().getBodyLocationImages().add(imageId);
 	}
-	
-	public Boolean checkBodyImageNumber(Patient patient)
+
+	public int checkBodyImageNumber()
 	{
-		BodyImages = patient.getBodyLocationImages();
-		if(BodyImages.size() >= 2){
-			return Boolean.TRUE;
-		}
-		else{
-			return Boolean.FALSE;
-		}
+		BodyImages = MockDataRepositoryUserManager.GetPatient().getBodyLocationImages();
+		return BodyImages.size();
 	}
-	public void deleteBodyLocationImage(Patient patient, String ImageId)
+	public void deleteBodyLocationImage(String ImageId)
 	{
 		//missing part: delete image from database
-        BodyImages = DataRepositorySingleton.getUser("").getBodyLocationImages();
-        for(int i = 0; i<BodyImages.size();i++){
-            if(BodyImages.get(i).equals(ImageId)){
-                BodyImages.remove(i);
-            }
-        }
-        DataRepositorySingleton.getUser("").setBodyLocationImageIds(BodyImages);
+		BodyImages = MockDataRepositoryUserManager.GetPatient().getBodyLocationImages();
+		for(int i = 0; i<BodyImages.size();i++){
+			if(BodyImages.get(i).equals(ImageId)){
+				BodyImages.remove(i);
+			}
+		}
+		MockDataRepositoryUserManager.GetPatient().setBodyLocationImageIds(BodyImages);
 	}
 }
