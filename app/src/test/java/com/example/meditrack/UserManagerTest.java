f3d007d3c855ff7c91a166b5eb8929f9112c0307
RiXu2 <35273575+RiXu2@users.noreply.github.com>
2018-11-15 18:06:11 -0700

    Update UserManagerTest.java

--- app/src/test/java/com/example/meditrack/UserManagerTest.java
+++ app/src/test/java/com/example/meditrack/UserManagerTest.java
@@ -7,30 +7,32 @@ public class UserManagerTest {
     @Test
     public void testUserManager(){
 
+
         // missing part: test addBodyLocationImage(), didn't do the body location image part yet
         String email = "Email@ualberta.ca";
         String phoneNumber = "7806554738";
         UserManager userManager = new UserManager();
-        userManager.EditContactInfo(email, phoneNumber);
+        userManager.EditContactInfo(MockDataRepositoryUserManager.GetPatient(), email, phoneNumber);
         Patient patient = MockDataRepositoryUserManager.GetPatient();
 
         //check EditContactInfo()
         Assert.assertTrue("Edit method does not work in right way", email.equals(patient.getContactInfo().getEmail()));
 
         //check checkBodyImageNumber()
-        Assert.assertTrue("check image number method does not work in right way", 3 == userManager.checkBodyImageNumber());
+        Assert.assertTrue("check image number method does not work in right way", 3 == userManager.checkBodyImageNumber(MockDataRepositoryUserManager.GetPatient()));
 
         //check deleteBodyLocationImage()
-        userManager.deleteBodyLocationImage("Img1224");
-        Assert.assertTrue("delete image method does not work in right way",2 == userManager.checkBodyImageNumber());
+        userManager.deleteBodyLocationImage(MockDataRepositoryUserManager.GetPatient(),"Img1224");
+        Assert.assertTrue("delete image method does not work in right way",2 == userManager.checkBodyImageNumber(MockDataRepositoryUserManager.GetPatient()));
 
         //check addPatient()
         String id = "22345878";
         int lengthOfList = MockDataRepositoryUserManager.GetCareProvider().getPatientIds().size();
-        userManager.addPatient(id);
+        userManager.addPatient(MockDataRepositoryUserManager.GetCareProvider(),id);
         int new_length = MockDataRepositoryUserManager.GetCareProvider().getPatientIds().size();
         Assert.assertTrue("add patient method does not work in right way", lengthOfList+1 == new_length );
 
     }
 
 }
+
