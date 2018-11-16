c58cba4c77c7d2eb1b4f917d598945c1503f2bdc
RiXu2 <35273575+RiXu2@users.noreply.github.com>
2018-11-16 14:27:31 -0700

    Update MockDataRepositoryUserManager.java

--- app/src/main/java/com/example/meditrack/MockDataRepositoryUserManager.java
+++ app/src/main/java/com/example/meditrack/MockDataRepositoryUserManager.java
@@ -4,24 +4,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MockDataRepositoryUserManager extends DataRepositorySingleton {
-    private static Patient patient;
-    private static CareProvider careProvider;
 
-    public static Patient GetPatient()
+    public Patient GetPatient()
     {
         String email = "testEmail@ualberta.ca";
         String phoneNumber = "7808001557";
         ArrayList<String> bodyImageId = new ArrayList<String>(Arrays.asList("Img1223","Img1224","Img1225"));
         ContactInfo contactInfo = new ContactInfo(email,phoneNumber);
-        patient.setContactInfo(contactInfo);
-        patient.setBodyLocationImageIds(bodyImageId);
+        Patient patient = new Patient("12345678",bodyImageId,contactInfo);
         return patient;
     }
 
-    public static CareProvider GetCareProvider()
+    public CareProvider GetCareProvider()
     {
         ArrayList<String> patientId = new ArrayList<String>(Arrays.asList("12345678","22345678","32345678"));
-        careProvider.setPatientIds(patientId);
+        CareProvider careProvider = new CareProvider("10074289",patientId);
         return careProvider;
     }
 }
