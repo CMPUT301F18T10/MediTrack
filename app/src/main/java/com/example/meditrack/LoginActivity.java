edfc2fedf748886baff869ddb94579b5b3c88197
Yuanmao Zhu (Kris) <32859592+yuanmaoKris@users.noreply.github.com>
2018-11-27 00:16:56 -0700

    slightly change the UI; add register button; hidden security tokens

--- app/src/main/java/com/example/meditrack/LoginActivity.java
+++ app/src/main/java/com/example/meditrack/LoginActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import static com.example.meditrack.ApplicationManager.UserMode.CareGiver;
 
@@ -22,30 +23,27 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
     public void loginClick(View v){
-
         mUserEmailEdit = (EditText) findViewById(R.id.loginEmail);
         mUserEmail = mUserEmailEdit.getText().toString();
         mCaretakerRadio = (RadioButton) findViewById(R.id.caretakerRadioButton);
-
         // TODO: We need exception handling at this stage
         if(mCaretakerRadio.isChecked()){
             mApplicationManager = new ApplicationManager(ApplicationManager.UserMode.CareGiver);
             if (mApplicationManager.DoesUserExist(mUserEmail))
-            {
-                // Log in
+            {   // Log in
                 mApplicationManager.LogIn(mUserEmail, DataRepositorySingleton.GetInstance());
+                Intent caretakerIntent = new Intent(LoginActivity.this, PatientsListActivity.class);
+                caretakerIntent.putExtra("caretakerID", mUserEmail);
+                startActivity(caretakerIntent);
             }
-            else
-            {
-                // Register user and then log in
-                mApplicationManager.RegisterUser(mUserEmail);
-                mApplicationManager.LogIn(mUserEmail, DataRepositorySingleton.GetInstance());
+            else{
+                Toast.makeText(this, "This User Email Is Not Registered, Please Register it!",Toast.LENGTH_LONG).show();
             }
-            Intent caretakerIntent = new Intent(LoginActivity.this, PatientsListActivity.class);
-            caretakerIntent.putExtra("caretakerID", mUserEmail);
-            startActivity(caretakerIntent);
+
+
         }
         else{
             mApplicationManager = new ApplicationManager(ApplicationManager.UserMode.Patient);
@@ -53,18 +51,53 @@ public class LoginActivity extends AppCompatActivity {
             {
                 // Log in
                 mApplicationManager.LogIn(mUserEmail, DataRepositorySingleton.GetInstance());
+                Intent caretakerIntent = new Intent(LoginActivity.this,ProblemsListActivity.class);
+                caretakerIntent.putExtra("patientID", mUserEmail);
+                startActivity(caretakerIntent);
+            }
+
+            else
+            {// Register user and then log in
+                Toast.makeText(this, "This User Email Is Not Registered, Please Register it!",Toast.LENGTH_LONG).show();
+            }
+
+        }
+
+
+    }
+    public void RegisterClick(View v){
+        mUserEmailEdit = (EditText) findViewById(R.id.loginEmail);
+        mUserEmail = mUserEmailEdit.getText().toString();
+        mCaretakerRadio = (RadioButton) findViewById(R.id.caretakerRadioButton);
+        if (mCaretakerRadio.isChecked()) {
+            mApplicationManager = new ApplicationManager(ApplicationManager.UserMode.CareGiver);
+            // Register user and then log in
+            if (mApplicationManager.DoesUserExist(mUserEmail)){
+                Toast.makeText(this, "This User Email Has Registered, Please Login it!",Toast.LENGTH_LONG).show();
             }
             else
             {
-                // Register user and then log in
                 mApplicationManager.RegisterUser(mUserEmail);
                 mApplicationManager.LogIn(mUserEmail, DataRepositorySingleton.GetInstance());
+                Intent patientIntent = new Intent(LoginActivity.this, PatientsListActivity.class);
+                patientIntent.putExtra("caretakerID", mUserEmail);
+                startActivity(patientIntent);
+
             }
-            Intent patientIntent = new Intent(LoginActivity.this, ProblemsListActivity.class);
-            patientIntent.putExtra("patientID", mUserEmail);
-            startActivity(patientIntent);
+        }
+        else{
+            mApplicationManager = new ApplicationManager(ApplicationManager.UserMode.Patient);
+            if(mApplicationManager.DoesUserExist(mUserEmail)){
+                Toast.makeText(this, "This User Email Has Registered, Please Login it!",Toast.LENGTH_LONG).show();
+            }
+            else{
+                mApplicationManager.RegisterUser(mUserEmail);
+                mApplicationManager.LogIn(mUserEmail, DataRepositorySingleton.GetInstance());
+                Intent patientIntent = new Intent(LoginActivity.this, ProblemsListActivity.class);
+                patientIntent.putExtra("patientID", mUserEmail);
+                startActivity(patientIntent);
+            }
         }
 
     }
-
 }
