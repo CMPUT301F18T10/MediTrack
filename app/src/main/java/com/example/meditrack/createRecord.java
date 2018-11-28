2742e83fe2b5d10cf5e69050f95d5e445c4af30e
JohnYujj <35233051+JohnYujj@users.noreply.github.com>
2018-11-27 18:42:18 -0700

    fixed ui and names for creating record

---
+++ app/src/main/java/com/example/meditrack/createRecord.java
@@ -0,0 +1,32 @@
+package com.example.meditrack;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class createRecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_record);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void createRecordBrowse() {
+
+    }
+    public void createRecordAddPhoto(){
+
+    }
+    public void createRecordAddGPS() {
+
+    }
+    public void createRecordAddBody(){
+
+    }
+    public void createRecordFinish(){
+        
+    }
+}
