84da9019a550077fa7622d070e59bc5750a5d0d4
hdhindsa <harshdhindsa07@gmail.com>
2018-12-03 10:10:37 -0700

    Update searchActivity.java

--- app/src/main/java/com/example/meditrack/searchActivity.java
+++ app/src/main/java/com/example/meditrack/searchActivity.java
@@ -1,6 +1,7 @@
 package com.example.meditrack;
 
 import android.content.Intent;
+import android.icu.text.AlphabeticIndex;
 import android.opengl.EGL14;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -40,8 +41,79 @@ public class searchActivity extends AppCompatActivity {
         else if(GPSRadio.isChecked()){
             //searchmanager GPSsearch record
         }
+
+
+
         else if(recordRadio.isChecked()){
-            //searchmanager recordsearch by keyword
+            //searchmanager recordsearch by keyword[
+            ArrayList<String> recordSearchList = new ArrayList<>();
+            Intent intent = getIntent();
+            String PatientId = intent.getStringExtra("patientID");
+            ArrayList Problems = mDRS.GetProblemsForPatientId(PatientId);
+            String PatientRecordTitle = new String();
+            String PatientRecordDescription = new String();
+            String CareGiverComment = new String();
+            EditText RecordKeywordInput = this.findViewById(R.id.searchInput);
+            String RecordKeyword = RecordKeywordInput.getText().toString();
+            ArrayList ProblemIds = new ArrayList<String>();
+            ArrayList RecordIds = new ArrayList<String>();
+            ArrayAdapter<String> RecordSearchadapter;
+
+
+            for(int i = 0; i<Problems.size();i++){
+                String tempProblemId = mDRS.GetProblemsForPatientId(PatientId).get(i).getId();
+                String tempProblemTitle = mDRS.GetProblemsForPatientId(PatientId).get(i).getTitle();
+
+                ArrayList PatientRecords = mDRS.GetPatientRecordsForProblem(tempProblemId);
+               // ArrayList CareGiverRecords = mDRS.GetCareGiverRecordsForProblemId(tempProblemId);
+                for(int j = 0; j<PatientRecords.size();j++){
+                    PatientRecordTitle = mDRS.GetPatientRecordsForProblem(tempProblemId).get(j).getTitle();
+                    PatientRecordDescription =  mDRS.GetPatientRecordsForProblem(tempProblemId).get(j).getDescription();
+                    String tempRecordId = mDRS.GetPatientRecordsForProblem(tempProblemId).get(j).getId();
+
+                    if(PatientRecordTitle.toLowerCase().contains(RecordKeyword.toLowerCase()) || PatientRecordDescription.toLowerCase().contains(RecordKeyword.toLowerCase())){
+                        ProblemIds.add(tempProblemId);
+                        RecordIds.add(tempRecordId);
+                        recordSearchList.add(tempProblemTitle+" : "+PatientRecordTitle);
+                    }
+                }
+
+            }
+            for(int i = 0; i<Problems.size();i++){
+                String tempProblemId = mDRS.GetProblemsForPatientId(PatientId).get(i).getId();
+
+                //ArrayList PatientRecords = mDRS.GetPatientRecordsForProblem(tempProblemId);
+                ArrayList CareGiverRecords = mDRS.GetCareGiverRecordsForProblemId(tempProblemId);
+                for(int j = 0; j<CareGiverRecords.size();j++){
+                    CareGiverComment = mDRS.GetCareGiverRecordsForProblemId(tempProblemId).get(j).getCareProviderComment();
+                    String tempRecordId = mDRS.GetCareGiverRecordsForProblemId(tempProblemId).get(j).getId();
+
+                    if(CareGiverComment.toLowerCase().contains(RecordKeyword.toLowerCase())){
+                        ProblemIds.add(tempProblemId);
+                        RecordIds.add(tempRecordId);
+                        recordSearchList.add("Care Giver Comment :  "+CareGiverComment);
+                    }
+                }
+
+            }
+            //recordSearchList.add("headache");
+            ListView listView = findViewById(R.id.searchRecordListView);
+
+            RecordSearchadapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, recordSearchList);
+            listView.setAdapter(RecordSearchadapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    Intent intent = new Intent(searchActivity.this, viewRecordActivity.class);
+                    intent.putExtra("problemId", ProblemIds.get(position).toString());
+                    intent.putExtra("recordId",RecordIds.get(position).toString());
+
+                    startActivity(intent);
+                }
+            });
+
         }
 
 
@@ -49,12 +121,14 @@ public class searchActivity extends AppCompatActivity {
             //searchmanager problemsearch by keyword
             ArrayList<String> problemSearchList = new ArrayList<>();
 
-            String ProblemTitle = new String();
-            String ProblemDescription = new String();
+            String RecordTitle = new String();
+            String RecordDescription = new String();
             String tempProblemId = new String();
+            String tempRecordId = new String();
             String PatientId = new String();
             Intent intent = getIntent();
             ArrayList ProblemIds = new ArrayList<String>();
+            ArrayList RecordIds = new ArrayList<String>();
 
             ArrayAdapter<String> ProblemSearchadapter;
             ArrayList Problems = new ArrayList<Problem>();
@@ -89,13 +163,14 @@ public class searchActivity extends AppCompatActivity {
             Problems = mDRS.GetProblemsForPatientId(PatientId);
 
             for(int i = 0; i<Problems.size();i++){
-                ProblemTitle = mDRS.GetProblemsForPatientId(PatientId).get(i).getTitle();
-                ProblemDescription =  mDRS.GetProblemsForPatientId(PatientId).get(i).getDescription();
-                tempProblemId = mDRS.GetProblemsForPatientId(PatientId).get(i).getId();
+                RecordTitle = mDRS.GetPatientRecordsForProblem(PatientId).get(i).getTitle();
+                RecordDescription =  mDRS.GetPatientRecordsForProblem(PatientId).get(i).getDescription();
+                tempRecordId = mDRS.GetPatientRecordsForProblem(PatientId).get(i).getId();
 
-                if(ProblemTitle.toLowerCase().contains(problemKeyword.toLowerCase()) || ProblemDescription.toLowerCase().contains(problemKeyword.toLowerCase())){
+                if(RecordTitle.toLowerCase().contains(problemKeyword.toLowerCase()) || RecordDescription.toLowerCase().contains(problemKeyword.toLowerCase())){
                     ProblemIds.add(tempProblemId);
-                    problemSearchList.add(ProblemTitle);
+                    RecordIds.add(tempRecordId);
+                    problemSearchList.add(RecordTitle);
                 }
 
             }
