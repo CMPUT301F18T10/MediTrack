580ad5195da0ef556472a28e84582e336b82483d
RiXu2 <35273575+RiXu2@users.noreply.github.com>
2018-12-02 23:21:25 -0400

    Update ProfileInformationActivity.java

--- app/src/main/java/com/example/meditrack/ProfileInformationActivity.java
+++ app/src/main/java/com/example/meditrack/ProfileInformationActivity.java
@@ -51,89 +51,8 @@ public class ProfileInformationActivity extends AppCompatActivity {
         phoneNumberEdit.setText(patient.getContactInfo().getPhoneNumber());
         emailAddressEdit.setText(patient.getContactInfo().getEmail());
 
-        cpIds = patient.getCareProviderId();
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, cpIds);
-        caretakerList.setAdapter(adapter);
-
-        caretakerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // remove item from data and update listView
-                AlertDialog.Builder adb = new AlertDialog.Builder(getApplicationContext());
-                adb.setTitle("Delete?");
-                adb.setMessage("Are you sure you want to delete " + position);
-                final int pos = position;
-                adb.setNegativeButton("Cancel", null);
-                adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        cpIds.remove(pos);
-                        patient.setCareProviderId(cpIds);
-                        adapter.notifyDataSetChanged();
-                    }
-                });
-                adb.show();
-
-            }
-        });
-        ListView imageList = findViewById(R.id.profileImageListListView);
-        imageList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // remove and update
-
-            }
-        });
-
     }
 
-    public void addBodyImage(View v) {
-    }
-
-    public void addCaretaker(View v) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
-        builder.setTitle("Add Caretaker");
-        final EditText input = new EditText(getApplicationContext());
-        input.setInputType(InputType.TYPE_CLASS_TEXT);
-        builder.setView(input);
-
-        ListView caretakerList = findViewById(R.id.profileCaretakerListListView);
-        ArrayList<String> cpIds;
-
-        Intent intent = getIntent();
-        String patientID = intent.getStringExtra("patientID");
-        try {
-            patient = dataRepositorySingleton.GetPatientForId(patientID);
-        } catch (ItemNotFound itemNotFound) {
-            itemNotFound.printStackTrace();
-        }
-        cpIds = patient.getCareProviderId();
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, cpIds);
-        caretakerList.setAdapter(adapter);
-
-        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                CaretakerAdd = input.getText().toString();
-                // check if caretaker exists, if so, add this patient to caretaker list
-
-                ApplicationManager.UserMode userMode = ApplicationManager.UserMode.CareGiver;
-                ApplicationManager applicationManager = new ApplicationManager(userMode);
-                if (applicationManager.DoesUserExist(CaretakerAdd) == true) {
-                    cpIds.add(CaretakerAdd);
-                    patient.setCareProviderId(cpIds);
-                    adapter.notifyDataSetChanged();
-                }
-                ;
-            }
-        });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-    }
 
     public void profileSave(View v) throws DataRepositorySingleton.DataRepositorySingletonNotInitialized, DataRepositorySingleton.InvalidUserMode
     {
